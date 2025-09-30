e96075cfb59a7dea6ffecf9ecef038e53e28a031
Crazy_Fox <holmyliszag@ukr.net>
2025-09-30 19:27:41 +0300

    first fraction hw

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,25 +1,19 @@
 import axios from 'axios';
 
 export default function getImagesByQuery(query) {
-  // Перетворення пошукового запиту з декількох слів
-  // у відповідний формат
-
-  let searchWords = query.split(' ').join('+');
-
   // Створення параметрів запиту
   const searchParams = new URLSearchParams({
     key: '52540887-918f2903ad65f90a6d5993705',
-    q: searchWords,
+    q: query,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
     per_page: 9,
   });
-  axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
   //Відправка запиту
-  axios
-    .get(`https://pixabay.com/api?${searchParams}`)
+  return axios
+    .get(`https://pixabay.com/api/?${searchParams}`)
     .then(response => response.data.hits)
     .catch(error => console.log(error));
 }
--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@
 
 // Імпорти бібліотек
 
-import axios from 'axios';
-// axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
@@ -14,15 +12,19 @@ import getImagesByQuery from './js/pixabay-api';
 // Обробка сабміту форми
 
 const form = document.querySelector('.form');
-const answer = form.addEventListener('submit', startSearch);
+form.addEventListener('submit', startSearch);
 
 function startSearch(event) {
   event.preventDefault();
   const searchWords = event.target.elements.word.value.trim();
   if (searchWords != '') {
-    getImagesByQuery(searchWords);
+    // Запит та обробка його результату
+
+    getImagesByQuery(searchWords).then(value => console.log(value));
     form.reset();
   } else {
+    // Попередження при порожньому запиті
+
     iziToast.warning({
       message: 'Enter word for search...',
       messageColor: '#fff',
@@ -32,6 +34,3 @@ function startSearch(event) {
     });
   }
 }
-
-// Робота з отриманими даними
-console.log(answer);
